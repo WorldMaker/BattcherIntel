cbd410c7ea4b51890cda448b51bbeedb5ea8c4eb
Max Battcher <me@worldmaker.net>
2013-12-13 01:36:03 -0500

    Basic rebranding things

--- BattcherIntel/App/main.js
+++ BattcherIntel/App/main.js
@@ -15,7 +15,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Starter Kit';
+    app.title = 'Battcher Intelligence Agency';
 
     app.configurePlugins({
         router: true,
