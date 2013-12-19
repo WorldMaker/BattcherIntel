daa774a42554714eaea7f4483493bb2ad4952f83
Max Battcher <me@worldmaker.net>
2013-12-18 23:16:17 -0500

    Oh right, CLI

--- deploy.cmd
+++ deploy.cmd
@@ -74,10 +74,10 @@ IF /I "BattcherIntel.sln" NEQ "" (
 )
 
 :: 2.a. Grunt
-call npm install -g grunt
+call npm install -g grunt-cli
 call npm install --saveDev
-SET GRUNT_CMD=node "%appdata%\npm\node_modules\grunt\bin\grunt"
-call %GRUNT_CMD%
+:: SET GRUNT_CMD=node "%appdata%\npm\node_modules\grunt\bin\grunt"
+call grunt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build to the temporary path
